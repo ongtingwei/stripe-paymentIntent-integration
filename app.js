88d785a974c062a4c46a5e0399d7eb900b52283c
Ting Wei <ongtingwei@live.com>
2026-06-23 00:21:01 -0700

    Initial commit

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 require('dotenv').config();
-const stripe = require('stripe');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const { v4: uuidv4 } = require('uuid');
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+const paymentStatus = {};
 
 var app = express();
 
@@ -14,6 +17,41 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }))
+
+
+app.post('/webhook', express.raw({ type: 'application/json' }),function(request, response) {
+  const sig = request.headers['stripe-signature'];
+  const body = request.body;
+
+  let event = null;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    // invalid signature
+    response.status(400).end();
+    return;
+  }
+
+  let intent = null;
+  switch (event['type']) {
+    case 'payment_intent.succeeded':
+      intent = event.data.object;
+      paymentStatus[intent.id] = true;
+      console.log("Succeeded:", intent.id);
+
+      break;
+    case 'payment_intent.payment_failed':
+      intent = event.data.object;
+      const message = intent.last_payment_error && intent.last_payment_error.message;
+      console.log('Failed:', intent.id, message);
+      break;
+  }
+
+  response.sendStatus(200);
+});
+
+
 app.use(express.json({}));
 
 /**
@@ -53,15 +91,59 @@ app.get('/checkout', function(req, res) {
   res.render('checkout', {
     title: title,
     amount: amount,
-    error: error
+    error: error,
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
   });
 });
+
+app.post('/create-payment-intent', async function(req,res){
+  const {amount} = req.body;
+
+  const idempotencyKey = uuidv4();
+  console.log('Idempotency key:', idempotencyKey);
+  
+  try{
+    const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    automatic_payment_methods: {
+      enabled: true,
+      },
+    },{
+      idempotencyKey: idempotencyKey
+    }
+    );
+    res.json({clientSecret:paymentIntent.client_secret});
+  }catch (err){
+    res.status(400).json({error : err.message });
+  }
+
+}
+)
 
 /**
  * Success route
  */
-app.get('/success', function(req, res) {
-  res.render('success');
+app.get('/success', async function(req, res) {
+  const {payment_intent, email} = req.query;
+  await new Promise(resolve => setTimeout(resolve, 1000)); // add loading bar to UI for enhancement
+    
+
+  if (!paymentStatus[payment_intent]) {
+    return res.render('success', {error:'Opps! There is an issue with your payment'});
+  }
+
+  try{
+    const paymentIntent = await stripe.paymentIntents.retrieve(payment_intent);
+    res.render('success',{
+      paymentIntentId: paymentIntent.id,
+      amount: paymentIntent.amount,
+      currency: paymentIntent.currency,
+      email: email,
+    })
+  } catch (err){
+  res.render('success', {error: err.message});
+  }
 });
 
 /**
--- public/js/custom.js
+++ public/js/custom.js
@@ -3,12 +3,60 @@
  */
 
 $(document).ready(function() {
-  var amounts = document.getElementsByClassName("amount");
+  const amounts = document.getElementsByClassName("amount");
 
   // iterate through all "amount" elements and convert from cents to dollars
-  for (var i = 0; i < amounts.length; i++) {
-    amount = amounts[i].getAttribute('data-amount') / 100;  
+  for (let i = 0; i < amounts.length; i++) {
+    let amount = amounts[i].getAttribute('data-amount') / 100;  
     amounts[i].innerHTML = amount.toFixed(2);
   }
+
+  //Run logic only on checkout page. check if the container if match checkout-container
+  const container = document.getElementById("checkout-container");
+  if (!container) return;
+
+  const publishableKey = container.getAttribute('data-publishable-key');
+  const amount = parseInt(container.getAttribute('data-amount'))
+
+  //Initialize Stripe
+  const stripe = Stripe(publishableKey);
+  let elements;
+
+  //Create PaymentIntent and mount Payment Element once page is loaded
+  fetch('/create-payment-intent', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({amount:amount})
+  })
+  .then(function(res) { return res.json();})
+  .then(function(data){
+    if (data.error) {
+      document.getElementById('payment-errors').textContent = data.error;
+      return;
+    }
+    elements= stripe.elements({clientSecret: data.clientSecret});
+    const paymentElement = elements.create('payment');
+    paymentElement.mount('#payment-element')
+  });
+
+  //When user clicks submit, call stripe.confirmPayment directly from browser
+  const form = document.querySelector('form[name="payment-form"]');
+  form.addEventListener('submit', async function(e){
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+
+    const result = await stripe.confirmPayment({
+      elements: elements,
+      confirmParams:{
+        return_url: window.location.origin + '/success?email=' + encodeURIComponent(email)
+      }
+    });
+    if (result.error) {
+      document.getElementById('payment-errors').textContent = result.error.message;
+    }
+  })
 })
 
